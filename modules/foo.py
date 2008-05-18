b0dccf28403510842374acd1a599ab4d3abe8635
stick <stick@spectra.kolej.mff.cuni.cz>
2008-05-18 17:34:28 +0200

    added AUTHORS file minor changes to code added foo module - will be used as module template (change it to reflect the API)

---
+++ modules/foo.py
@@ -0,0 +1,10 @@
+class Info(object):
+
+    name = "foo"
+    descr = "template module"
+
+    @classmethod
+    def main(cls, argv):
+        """ a main method """
+
+        return "TODO: not implemented yet, just for test of plugin loader :)"
