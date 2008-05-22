25a646b07029785024638cb938efc99716a7b93e
Michal Vyskocil <mvyskocil@zelva.suse.cz>
2008-05-22 14:19:24 +0200

    Initial support of SQL database

--- scout.py
+++ scout.py
@@ -1,7 +1,8 @@
-#!/usr/bin/python
-
 import os
 import sys
+import sqlite3
+
+# this is a comment
 
 class CommandLineParser(object):
     """
@@ -18,12 +19,12 @@ class CommandLineParser(object):
 
         module.foo()
     """
-    @classmethod
-    def __init__(cls, modules = None):
+
+    def __init__(self, modules = None):
         """
         A constructor for CommandLineParser, argument modules should be inicialized
         """
-        cls.modules = dict()
+        self.modules = dict()
 
         if modules != None:
             _modules = modules
@@ -31,50 +32,43 @@ class CommandLineParser(object):
                 _modules = (modules, )
 
             for m in _modules:
-                cls.add_module(m)
+                self.add_module(m)
 
-        cls.prog = os.path.basename(sys.argv[0])
+        self.prog = sys.argv[0]
 
-    @classmethod
-    def add_module(cls, module):
-        cls.modules[module.ScoutModule.name] = module
+    def add_module(self, module):
+        self.modules[module.Info.name] = module
 
-    @classmethod
-    def _is_help(cls, arg):
-        if arg in ("help", "-h", "-help", "--help"):
-            return True
+    def _is_help(self, arg):
+        for help in ("help", "-h", "-help", "--help"):
+            if arg == help:
+                return True
         return False
 
-    @classmethod
-    def print_usage(cls):
-        print("")
-        print("Usage: %s <module> <search_term> [module-options]" % (cls.prog))
-        print("       - or -")
-        print("       %s help <module> for module help" % (cls.prog))
+    def print_usage(self):
+        print("Usage: %s module searchTerm" % (self.prog))
+        print("       %s help module or %s module help for help" % (self.prog, self.prog))
         print("")
         print("available modules:")
-        for module in cls.modules.values():
-            print("\t%s\t%s" % (module.ScoutModule.name, module.ScoutModule.desc))
-        print("")
+        for module in self.modules.values():
+            print("\t%s\t%s" % (module.Info.name, module.Info.descr))
+
         sys.exit(1)
 
 
-    @classmethod
-    def module_not_found(cls, name):
-        print "Module '%s' not found" % (name)
-        sys.exit(1)
+    def parse(self):
 
-    @classmethod
-    def parse(cls):
-
-        if len(sys.argv) == 1 or (len(sys.argv) == 2 and cls._is_help(sys.argv[1])):
-            cls.print_usage()
+        if len(sys.argv) == 1 or (len(sys.argv) == 2 and self._is_help(sys.argv[0])):
+            self.print_usage()
 
         mname = sys.argv[1]
-        if not mname in cls.modules:
-            cls.module_not_found(mname)
-        del sys.argv[1]
-        return cls.modules[mname]
+        if not mname in self.modules:
+            # fixme - and exception?
+            self.print_module_error(mname)
+
+        del sys.argv[1]         # delete a module name for argument list
+        return self.modules[mname]
+
 
 class ModuleLoader(object):
     """
@@ -84,36 +78,152 @@ class ModuleLoader(object):
     modules = list()
 
     @classmethod
-    def import_from(cls, dirs):
+    def import_modules(cls, dirs):
+        ret = list()
         _dirs = dirs
+
         # make an non-iter item as iter
         if not hasattr(dirs, "__iter__"):
             _dirs = (dirs, )
+            
         for dir in _dirs:
-            cls._import(dir)
+            ret.extend(cls._import(dir))
+        
+        return ret
 
     @classmethod
     def _import(cls, dir):
-        if not os.path.isdir(dir):
+
+        adir = os.path.abspath(dir)
+
+        if not os.path.isdir(adir):
             raise AttributeError("%s is not a directory" % dir)
-        sys.path.append(dir)
-        for file in os.listdir(dir):
+
+        sys.path.append(adir)
+
+        for file in os.listdir(adir):
             module_name, ext = os.path.splitext(file)
             if ext == '.py':
+                #log.info('import %s' % module_name)
                 module = __import__(module_name)
                 cls.modules.append(module)
 
-class ScoutCore(object):
+        return cls.modules
 
-    @classmethod
-    def run(cls):
+class Database(object):
 
-        ml = ModuleLoader
-        ml.import_from(sys.path[0] + "/modules")
+    db = "db.sqlite"
 
-        clp = CommandLineParser(ml.modules)
-        module = clp.parse()
-        module.ScoutModule.main()
+    def __init__(self):
+        if not os.path.exists(self.__class__.db):
+            self.init()
 
-if __name__ == "__main__":
-    ScoutCore.run()
+        self.conn = sqlite3.connect(self.__class__.db)
+
+    def init(self):
+        self.conn = sqlite3.connect(self.__class__.db)
+        self.conn.execute("""
+CREATE TABLE distro (
+    id          INT AUTO_INCREMENT,
+    name        VARCHAR(50) NOT NULL UNIQUE,
+    PRIMARY KEY (id)
+);
+        """)
+        self.conn.execute("""
+CREATE TABLE rpm (
+    id          INT AUTO_INCREMENT,
+    name        VARCHAR(50) NOT NULL UNIQUE,
+    PRIMARY KEY(id)
+);        
+        """)
+        # FIXME: an imports
+        self.conn.execute("""insert into distro VALUES(0, 'openSUSE:10.3')""")
+        self.conn.execute("""insert into distro VALUES(1, 'Java:jpackage-1.7')""")
+        self.conn.commit()
+        self.conn.close()
+
+    def __del__(self):
+        self.conn.close()
+
+    def _clever_query_result(self, c):
+        ret = list()
+        for row in c:
+            if len(row) == 1:           #(2)
+                ret.append(row[0])
+            else:                       #(3)
+                ret.append(row)
+        if len(ret) == 1:                #(1)
+            return ret[0]
+        return ret
+
+    def execute(self, query, *args, **kwargs):
+        """
+        an abstract database query - but more clever
+
+        query - a query to proceed
+
+        there're two kinds of placeholders (like as original DB/API execute, but in more Pythonic way)
+        - question marks (qmark style):
+          execute("SELECT ham, spam FORM foo WHERE bar=? and baz=?", bar, baz)
+        - named placeholders (named style)
+          execute("SELECT ham, spam FORM foo WHERE bar=:bar and baz=:baz", bar='42', baz='42')
+
+        Note: its not possible to combine this two types of placeholders in one call!
+
+        return
+        (1) if the length of result is one, return one value
+        (2) if the collumn in result is only one, return a list of values
+        (3) else returns a list of tuples
+        
+        """
+
+        # ther's not possible to use both a args and a keyword args
+        assert(args!=None or kwargs!=None)
+
+        c = self.conn.cursor()
+        if args==None and kwargs==None:
+            c.execute(query)
+        elif args!=None:
+            c.execute(query, args)
+        else:
+            c.execute(query, kwargs)
+        ret = self._clever_query_result(c)
+        c.close()
+        return ret
+
+    def distros(self):
+        """ return a list of distributions available in a database """
+        return self.execute(""" SELECT name from distro;""")
+    
+    def has_distro(self, name):
+        """ returns if the database contains specific distribution """
+        return name in self.distros()
+
+    def get_distro_id(self, name):
+        """ return an ID of distribution """
+        ret = -1
+        ret = self.execute("""SELECT id from distro WHERE name=? """, name)
+#        c = self.conn.cursor()
+        # fixme - a filtering !!!
+#        c.execute("""SELECT id from distro WHERE name='%s' """ % (name))
+#        for row in c:
+#            ret = row[0]
+#        c.close()
+        return ret
+
+def main():
+    
+    modules = ModuleLoader.import_modules("modules")
+    parser = CommandLineParser(modules)
+
+    module = parser.parse()
+
+    ret = module.Info.main(sys.argv)
+
+    print ret
+
+#main()
+
+d = Database()
+for row in d.distros():
+    print d.get_distro_id(row), row
