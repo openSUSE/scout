55abec592c0228de06df5180defa3c1e841a1dbf
Pavol Rusnak <stick@gk2.sk>
2008-07-29 16:01:21 +0200

    zypp support in bin module: first changes

--- scout/bin.py
+++ scout/bin.py
@@ -3,34 +3,37 @@
 
 import scout
 import sys
-# import zypp
 
-# class ZyppParser(object):
-# 
-#     rebuild_cache = False
-# 
-#     @classmethod
-#     def list(cls):
-#         z = zypp.ZYppFactory_instance().getZYpp()
-#         repoManager = zypp.RepoManager()
-#         repos = repoManager.knownRepositories()
-# 
-#         for repo in repos:
-#             if not repo.enabled():
-#                 continue
-#             if cls.rebuild_cache and not repoManager.isCached(repo):
-#                repoManager.buildCache(repo)
-#             z.addResolvables(repoManager.createFromCache(repo).resolvables())
-# 
-#     print "Available items: %d" % ( z.pool().size() )
-#     for item in z.pool():
-#         print "* %s:%s-%s.%s\t(%s)" % ( item.resolvable().kind(),
-#                                         item.resolvable().name(),
-#                                         item.resolvable().edition(),
-#                                         item.resolvable().arch(),
-#                                         item.resolvable().repository().info().alias() )
+try:
+    zypp = __import__(zypp)
+except:
+    zypp = None
 
-paths = [ "/bin", "/sbin", "/usr/bin", "/usr/sbin", "/usr/games", "/opt/kde3/bin", "/opt/kde3/sbin", "/opt/gnome/bin", "/opt/gnome/sbin" ];
+class ZyppParser(object):
+
+    rebuild_cache = False
+    paths = ( '/bin', '/sbin', '/usr/bin', '/usr/sbin', '/usr/games', '/opt/kde3/bin', '/opt/kde3/sbin', '/opt/gnome/bin', '/opt/gnome/sbin' )
+
+    @classmethod
+    def list(cls):
+        z = zypp.ZYppFactory_instance().getZYpp()
+        repoManager = zypp.RepoManager()
+        repos = repoManager.knownRepositories()
+
+        for repo in repos:
+            if not repo.enabled():
+                continue
+            if cls.rebuild_cache and not repoManager.isCached(repo):
+                repoManager.buildCache(repo)
+            z.addResolvables(repoManager.createFromCache(repo).resolvables())
+
+        print "Available items: %d" % ( z.pool().size() )
+        for item in z.pool():
+            print "* %s:%s-%s.%s\t(%s)" % ( item.resolvable().kind(),
+                                            item.resolvable().name(),
+                                            item.resolvable().edition(),
+                                            item.resolvable().arch(),
+                                            item.resolvable().repository().info().alias() )
 
 class ScoutModule(object):
 
@@ -39,8 +42,9 @@ class ScoutModule(object):
 
     @classmethod
     def query_zypp(cls, term):
+        if zypp == None:
+            return None
         # TODO: implement
-        print "Querying zypp repositories is not yet implemented"
         return None
 
     @classmethod
@@ -59,8 +63,7 @@ class ScoutModule(object):
     def main(cls):
 
         p = scout.Parser(cls.name)
-        # TODO: do not add zypp repo for now (not implemented)
-        # p.add_repo('zypp')
+        p.add_repo('zypp')
         if not p.parse():
             return None
         term = p.args[0]
