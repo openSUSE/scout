3aed171ef3d77b70b7fb861fedf3f817d87e7ff1
Pavol Rusnak <stick@gk2.sk>
2008-08-24 14:18:49 +0200

    added library module (no data yet)

--- data-gen/gen-library.py
+++ data-gen/gen-library.py
@@ -5,14 +5,14 @@ import fnmatch
 
 def process(s):
     print 'Processing %s ...' % s,
-    hdrs = dict()
+    libs = dict()
     pkgs = dict()
     f = os.popen('lzma -c -d %s.txt.lzma' % s)
     g = open(s + '.sql', 'w')
 
     g.write('''
     BEGIN;
-    CREATE TABLE headers (id_hdr INT PRIMARY KEY NOT NULL, header VARCHAR(64) NOT NULL, id_pkg INT NOT NULL);
+    CREATE TABLE libraries (id_libraries INT PRIMARY KEY NOT NULL, library VARCHAR(64) NOT NULL, id_pkg INT NOT NULL);
     CREATE TABLE packages(id_pkg INT PRIMARY KEY NOT NULL, package VARCHAR(64) NOT NULL);
     ''')
 
@@ -21,10 +21,10 @@ def process(s):
         if not pkgs.has_key( line[0] ):
             pkgs[ line[0] ] = len(pkgs)
             g.write("INSERT INTO packages(id_pkg, package) VALUES(%s, '%s');\n" % (pkgs[ line[0] ], line[0]))
-        if not hdrs.has_key( line[0]+line[1] ):
-            hdrs[ line[0]+line[1] ] = len(hdrs)
-            g.write("INSERT INTO headers(id_hdr, header, id_pkg) VALUES(%s, '%s', %s);\n" % ( hdrs[line[0]+line[1]], line[1], pkgs[ line[0] ]))
-    g.write('CREATE INDEX headers_header_idx ON headers(header);\n')
+        if not libs.has_key( line[0]+line[1] ):
+            libs[ line[0]+line[1] ] = len(libs)
+            g.write("INSERT INTO libraries(id_lib, header, id_pkg) VALUES(%s, '%s', %s);\n" % ( libs[line[0]+line[1]], line[1], pkgs[ line[0] ]))
+    g.write('CREATE INDEX libraries_library_idx ON headers(library);\n')
 
     g.write('COMMIT;\n')
     f.close()
@@ -32,5 +32,5 @@ def process(s):
     print 'done'
 
 for file in os.listdir('.'):
-    if fnmatch.fnmatch(file, 'header-*.txt.lzma'):
+    if fnmatch.fnmatch(file, 'library-*.txt.lzma'):
         process(file[:-9])
