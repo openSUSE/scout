6f71d097b75a15240ff9b72be3543f8ea3fd51f4
Michal Vyskocil <mvyskocil@suse.cz>
2008-08-26 11:21:57 +0200

    Convert autoconf and java modules to be based on the BasicCoutModule class.

--- scout/autoconf.py
+++ scout/autoconf.py
@@ -1,39 +1,12 @@
-# Copyright (c) 2008 Pavol Rusnak
+# Copyright (c) 2008 Pavol Rusnak. Michal Vyskocil
 # see __init__.py for license details
 
 import scout
 
-class ScoutModule(object):
+class ScoutModule(scout.BasicScoutModule):
 
     name = "autoconf"
     desc = "Search for autoconf macros inside the m4 files."
-
-    @classmethod
-    def query(cls, repo, term):
-        db = scout.Database(cls.name + '-' + repo)
-        r = db.query('SELECT package, m4, macro FROM macros LEFT JOIN m4s ON macros.id_m4=m4s.id_m4 LEFT JOIN packages ON m4s.id_pkg=packages.id_pkg WHERE macro LIKE ?', '%%%s%%' % term)
-        if r == None:
-            return None
-        if isinstance(r, list):
-            return map( lambda x: [repo] + list(x), r)
-        else:
-            return [ [repo] + list(r) ]
-        return r
-
-    @classmethod
-    def main(cls):
-
-        p = scout.Parser(cls.name)
-        if not p.parse():
-            return None
-        term = p.args[0]
-
-        result = scout.Result( ['repo', 'pkg', 'm4', 'macro'], ['repository', 'package', 'm4 file', 'macro']);
-
-        repos = p.get_repos()
-        if repos == None:
-            return None
-        for repo in repos:
-            result.add_rows( cls.query(repo, term) )
-
-        return result
+    sql = 'SELECT package, m4, macro FROM macros LEFT JOIN m4s ON macros.id_m4=m4s.id_m4 LEFT JOIN packages ON m4s.id_pkg=packages.id_pkg WHERE macro LIKE ?', '%%%s%%'
+    result_list = ['repo', 'pkg', 'm4', 'macro']
+    result_list2= ['repository', 'package', 'm4 file', 'macro']
--- scout/java.py
+++ scout/java.py
@@ -1,39 +1,12 @@
-# Copyright (c) 2008 Pavol Rusnak
+# Copyright (c) 2008 Pavol Rusnak, Michal Vyskocil
 # see __init__.py for license details
 
 import scout
 
-class ScoutModule(object):
+class ScoutModule(scout.BasicScoutModule):
 
     name = "java"
     desc = "Search for java classes inside the packaged JAR files."
-
-    @classmethod
-    def query(cls, repo, term):
-        db = scout.Database(cls.name + '-' + repo)
-        r = db.query('SELECT package, jar, class FROM classes LEFT JOIN jars ON classes.id_jar=jars.id_jar LEFT JOIN packages ON jars.id_pkg=packages.id_pkg WHERE class LIKE ?', '%%%s%%' % term)
-        if r == None:
-            return None
-        if isinstance(r, list):
-            return map( lambda x: [repo] + list(x), r)
-        else:
-            return [ [repo] + list(r) ]
-        return r
-
-    @classmethod
-    def main(cls):
-
-        p = scout.Parser(cls.name)
-        if not p.parse():
-            return None
-        term = p.args[0]
-
-        result = scout.Result( ['repo', 'pkg', 'jar', 'class'], ['repository', 'package', 'jar', 'class']);
-
-        repos = p.get_repos()
-        if repos == None:
-            return None
-        for repo in repos:
-            result.add_rows( cls.query(repo, term) )
-
-        return result
+    sql = 'SELECT package, jar, class FROM classes LEFT JOIN jars ON classes.id_jar=jars.id_jar LEFT JOIN packages ON jars.id_pkg=packages.id_pkg WHERE class LIKE ?'
+    result_list = ['repo', 'pkg', 'jar', 'class']
+    result_list2= ['repository', 'package', 'jar', 'class']
