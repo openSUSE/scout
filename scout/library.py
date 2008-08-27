ba80184981a06b58a3dfc0abdcc95e17d253740d
Pavol Rusnak <stick@gk2.sk>
2008-08-27 16:43:24 +0200

    sk translation minor changes

--- scout/library.py
+++ scout/library.py
@@ -6,5 +6,5 @@ import scout
 class ScoutModule(scout.BasicScoutModule):
 
     name = "library"
-    desc = _("Search for shared libraries")
+    desc = _("Search for shared libraries.")
     sql = 'SELECT package, library FROM libraries LEFT JOIN packages ON libraries.id_pkg=packages.id_pkg WHERE library LIKE ?'
