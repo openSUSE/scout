13bd2d6ea500c408f8d380d0cbf3c4e283205377
Alberto Planas <aplanas@gmail.com>
2018-07-24 11:56:37 +0200

    tests: Initial support for Python 3

--- test/database_test.py
+++ test/database_test.py
@@ -3,14 +3,14 @@
 # A tests for scout
 #
 
+import sqlite3
 import sys
+import unittest
+
 sys.path.insert(0, '../scout/')
-import __init__ as scout
+import scout
 if not hasattr(scout, 'ScoutCore'):
     raise ImportError('No main scout module found')
-
-import unittest
-import sqlite3
 
 
 class SimpleDatabaseTestCase(unittest.TestCase):
@@ -24,10 +24,12 @@ class SimpleDatabaseTestCase(unittest.TestCase):
     def setUp(self):
         self.database = scout.Database(':memory:')
 
-        c = self.database.conn.cursor()
+        self.database.begin()
+        c = self.database.cursor
 
         self.term = 'package3-module2'
-        self.sql_begin = "SELECT package, module FROM modules LEFT JOIN packages ON modules.id_pkg=packages.id_pkg"
+        self.sql_begin = 'SELECT package, module ' \
+            'FROM modules LEFT JOIN packages ON modules.id_pkg=packages.id_pkg'
 
         sql = """
 CREATE TABLE modules (id_modules INT PRIMARY KEY NOT NULL, module VARCHAR(64) NOT NULL, id_pkg INT NOT NULL);
@@ -35,7 +37,6 @@ CREATE TABLE packages(id_pkg INT PRIMARY KEY NOT NULL, package VARCHAR(64) NOT N
 INSERT INTO packages(id_pkg, package) VALUES(1, 'package1');
 INSERT INTO packages(id_pkg, package) VALUES(2, 'package2');
 INSERT INTO packages(id_pkg, package) VALUES(3, 'package3');
-
 INSERT INTO modules(id_modules, module, id_pkg) VALUES(1, 'package1-module1', 1);
 INSERT INTO modules(id_modules, module, id_pkg) VALUES(2, 'package1-module2', 1);
 INSERT INTO modules(id_modules, module, id_pkg) VALUES(3, 'package1-module3', 1);
@@ -45,8 +46,8 @@ INSERT INTO modules(id_modules, module, id_pkg) VALUES(6, 'package2-module3', 2)
 INSERT INTO modules(id_modules, module, id_pkg) VALUES(7, 'package3-module1', 3);
 INSERT INTO modules(id_modules, module, id_pkg) VALUES(8, 'package3-module2', 3);
 INSERT INTO modules(id_modules, module, id_pkg) VALUES(9, 'package3-module3', 3);
-
-CREATE INDEX modules_module_idx ON modules(module);"""
+CREATE INDEX modules_module_idx ON modules(module);
+"""
 
         for cmd in sql.splitlines():
             c.execute(cmd)
@@ -64,7 +65,7 @@ CREATE INDEX modules_module_idx ON modules(module);"""
         self.assertEqual(self.term, res[1])
 
     def testExecutePlaceholders1(self):
-        sql = self.sql_begin + " WHERE module LIKE ?"
+        sql = self.sql_begin + ' WHERE module LIKE ?'
 
         res = self.database.execute(sql, '%'+self.term+'%')
         self._check_simple_results_row(res)
@@ -73,9 +74,9 @@ CREATE INDEX modules_module_idx ON modules(module);"""
         self.assertEqual(self.term, res[1])
 
     def testExecutePlaceholders2(self):
-        sql = self.sql_begin + " WHERE module LIKE :module"
+        sql = self.sql_begin + ' WHERE module LIKE :module'
 
-        res = self.database.execute(sql, module='%' + self.term + '%')
+        res = self.database.execute(sql, module='%'+self.term+'%')
         self._check_simple_results_row(res)
 
         self.assertEqual(self.term.split('-')[0], res[0])
@@ -91,65 +92,64 @@ CREATE INDEX modules_module_idx ON modules(module);"""
         self.assertTrue(len(res) >= 0)
         self.assertTrue(type(res[0]), type(tuple()))
         self.assertEqual(len(res[0]), 2)
-        for i in xrange(len(res)):
-            self.assertEqual(type(res[i][0]), type(unicode()))
-            self.assertEqual(type(res[i][1]), type(unicode()))
+        for i in range(len(res)):
+            self.assertEqual(type(res[i][0]), type(str()))
+            self.assertEqual(type(res[i][1]), type(str()))
 
-        sql = "SELECT module FROM modules"
+        sql = 'SELECT module FROM modules'
 
         res = self.database.execute(sql)
 
         self.assertEqual(type(res), type(list()))
         self.assertTrue(len(res) >= 0)
-        for i in xrange(len(res)):
-            self.assertEqual(type(res[i]), type(unicode()))
+        for i in range(len(res)):
+            self.assertEqual(type(res[i]), type(str()))
 
-
-        sql = "SELECT module FROM modules WHERE module='package1-module1'"
+        sql = 'SELECT module FROM modules ' \
+            "WHERE module='package1-module1'"
 
         res = self.database.execute(sql)
-        self.assertEqual(type(res), type(unicode()))
+        self.assertEqual(type(res), type(str()))
         self.assertEqual(res, 'package1-module1')
 
     def testUsageOfBothPlaceholders(self):
 
-        sql = self.sql_begin + " WHERE module LIKE ? OR module LIKE :module"
+        sql = self.sql_begin + ' WHERE module LIKE ? OR module LIKE :module'
 
         try:
-            res = self.database.execute(sql, 'module', module='module')
-        except AssertionError, aserr:
+            self.database.execute(sql, 'module', module='module')
+        except AssertionError:
             pass
         else:
-            self.fail("The execute module disallows usage of both placeholders!!")
-
+            msg = 'The execute module disallows usage of both placeholders!'
+            self.fail(msg)
 
     def testIncorrectSQLCommand(self):
-
-        sql = "NOT_SQL_COMMAND"
+        sql = 'NOT_SQL_COMMAND'
 
         try:
-            res = self.database.execute(sql)
-        except sqlite3.OperationalError, operr:
+            self.database.execute(sql)
+        except sqlite3.OperationalError:
             pass
         else:
-            self.fail("The %s is not an SQL command" % sql)
+            self.fail('The %s is not an SQL command' % sql)
 
-        sql = self.sql_begin + " WHERE module LIKE ? "
+        sql = self.sql_begin + ' WHERE module LIKE ?'
         try:
-            res = self.database.execute(sql, module='module1')
-        except sqlite3.ProgrammingError, prgerr:
+            self.database.execute(sql, module='module1')
+        except sqlite3.ProgrammingError:
             pass
         else:
-            self.fail("The ? is not working with keyword args")
+            self.fail('The ? is not working with keyword args')
 
-        sql = self.sql_begin + " WHERE module LIKE :module "
+        sql = self.sql_begin + ' WHERE module LIKE :module'
         # FIXME!!! The sqlite3 doesn't return an error
-        #try:
-        #    res = self.database.execute(sql, 'module1')
-        #except sqlite3.ProgrammingError, prgerr:
-        #    pass
-        #else:
-        #    self.fail("The ? is not working with keyword args")
+        # try:
+        #     self.database.execute(sql, 'module1')
+        # except sqlite3.ProgrammingError:
+        #     pass
+        # else:
+        #     self.fail('The ? is not working with keyword args')
 
 
 suiteDatabase = unittest.makeSuite(SimpleDatabaseTestCase, 'test')
